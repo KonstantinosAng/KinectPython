880c1015ea1dbbcc4b56752f21a2ecf4241eb741
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-06-11 01:36:16 +0300

    Added bash file to start the ROS main node

---
+++ Gripper/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Always run setup.bash to initialize environment parameters that ROS requires.
+# Otherwise roslaunch will not find the files
+. ~/catkin_ws/devel/setup.bash
+# Assuming that the start.sh file has been called and ROS started correctly we can publish
+# to the hand node
+roslaunch reflex reflex_one.launch
+
