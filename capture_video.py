9843b88591bca1e56412251e3cb601e2874e8dc3
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-06-14 03:04:53 +0300

    added file to capture and save video

--- capture_video.py
+++ capture_video.py
@@ -1,4 +1,9 @@
 """
+Author: Konstantinos Angelopoulos
+Date: 04/02/2020
+All rights reserved.
+Feel free to use and modify and if you like it give it a star.
+
 Simultaneously capture video and store it.
 """
 
