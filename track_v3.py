6c4ed513fe19ceaaa8c4e780ace1f7f79c657b6b
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-06-14 03:12:09 +0300

    added main file

--- track_v3.py
+++ track_v3.py
@@ -1,4 +1,11 @@
-""" MAIN FILE """
+"""
+Author: Konstantinos Angelopoulos
+Date: 04/02/2020
+All rights reserved.
+Feel free to use and modify and if you like it give it a star.
+
+MAIN FILE
+"""
 
 
 import os
