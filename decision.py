762d7b24da33638301cb7c88f76d171bc3cdb5a8
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2020-06-14 03:05:50 +0300

    add deision model

--- decision.py
+++ decision.py
@@ -1,4 +1,9 @@
 """
+Author: Konstantinos Angelopoulos
+Date: 04/02/2020
+All rights reserved.
+Feel free to use and modify and if you like it give it a star.
+
 DECISION MODEL TO CALCULATE THE HUMAN DESIRED MOVEMENT
 """
 
